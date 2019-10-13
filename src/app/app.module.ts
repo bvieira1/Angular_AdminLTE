92223969f731d57a38c43507da91e82ff93ace3d
bvieira1 <cristianovieirati@gmail.com>
2019-10-12 23:33:18 -0300

    pROJETO EM ANGULAR E AdminLTE

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AuthGuard } from './guards/auth.guard';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+
+import { AppComponent } from './app.component';
+import { TokenInterceptor } from './interceptors/token.interceptors';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    AdminModule
+  ],
+  providers: [
+    AuthGuard,
+    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
